f8363c7193921cb060501b7cfae6db97be7d1107
Karl Mark Balagtey <karl.balagtey@gmail.com>
2019-08-16 21:18:11 +0100

    Added checkout page, checkout item component, updated react-router-dom to v5 and updated cart to use selectors from reselect

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,15 +1,17 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import { connect } from 'react-redux';
+import React from "react";
+import { Link } from "react-router-dom";
+import { connect } from "react-redux";
+import { createStructuredSelector } from "reselect";
 
-import { auth } from '../../firebase/firebase.utils';
+import { auth } from "../../firebase/firebase.utils";
 
-import CartIcon from '../cart-icon/cart-icon.component';
-import CartDropdown from '../cart-dropdown/cart-dropdown.component';
+import CartIcon from "../cart-icon/cart-icon.component";
+import CartDropdown from "../cart-dropdown/cart-dropdown.component";
+import { selectCartHidden } from "../../redux/cart/cart.selector";
+import { selectCurrentUser } from '../../redux/user/user.selector';
+import { ReactComponent as Logo } from "../../assets/crown.svg";
 
-import { ReactComponent as Logo } from '../../assets/crown.svg';
-
-import './header.styles.scss';
+import "./header.styles.scss";
 
 const Header = ({ currentUser, hidden }) => (
     <div className="header">
@@ -35,13 +37,12 @@ const Header = ({ currentUser, hidden }) => (
             <CartIcon />
         </div>
         {hidden ? null : <CartDropdown />}
-        
     </div>
 );
 
-const mapStateToProps = ({user: {currentUser}, cart: {hidden}}) => ({
-    currentUser,
-    hidden
+const mapStateToProps = createStructuredSelector({
+    currentUser: selectCurrentUser,
+    hidden: selectCartHidden
 });
 
 export default connect(mapStateToProps)(Header);
