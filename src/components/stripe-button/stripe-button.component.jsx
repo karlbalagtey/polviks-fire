c493e657e952dc58b6963806e8b027f9dfe194ad
Karl Mark Balagtey <karl.balagtey@gmail.com>
2019-08-22 17:57:24 +0100

    Integrate API with axios

--- src/components/stripe-button/stripe-button.component.jsx
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -1,13 +1,25 @@
 import React from "react";
 import StripeCheckout from "react-stripe-checkout";
+import axios from "axios";
 
 const StripeCheckoutButton = ({ price }) => {
     const priceForStripe = price * 100;
     const publishableKey = process.env.REACT_APP_STRIPE_PUB_KEY;
 
     const onToken = token => {
-        console.log(token);
-        alert("Payment Successful");
+        axios({
+            url: process.env.REACT_APP_API_URI + "payment/stripe",
+            method: "post",
+            data: {
+                amount: priceForStripe,
+                token
+            }
+        }).then(response => {
+            alert('Payment successful')
+        }).catch(error => {
+            console.log('Payment error: ', JSON.parse(error));
+            alert('There was an issue with your payment');
+        });
     };
 
     return (
